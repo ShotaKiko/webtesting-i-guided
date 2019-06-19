169fd141a5d12828a36b0b646acf9bc5e8228b0c
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-06-18 22:20:54 -0500

    experimented with testing simple add function using jest/initial commit

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,6 +2,17 @@ module.exports = {
   add,
 };
 
-function add() {
-  return null;
+// function add(a = 0, b = 0) {
+//   // a = a || 0
+//   return a + b;
+// }
+
+//reduce
+
+function add(x){
+  const args = Array.isArray(x) ? x : Array.from(arguments)
+
+  return args.reduce((acc, val) => {
+    return acc + val
+  }, 0)
 }
--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,34 @@
 const { add } = require('./calculator.js');
 
 // test away!
+describe('the add function', () => {
+    //test runner
+    it('should add two values', () => {
+        const result = add(2,2)
+
+        //assertion
+        expect(result).toBe(4)
+        expect(add(-1, 4)).toBe(3)
+        expect(add(0, 6)).toBe(6)
+    })
+
+    it('should return 0 as a default when no arguments are provided', () => {
+        expect(add()).toBe(0)
+    })
+
+    it('should return the passed in value if only one arg is provided', () => {
+        expect(add(22)).toBe(22)
+        expect(add(3)).toBe(3)
+    })
+
+    it('should be able to add any number of values', () => {
+        expect(add(1, 2, 3)).toBe(6)
+        expect(add(1, 2, 3, 10, 12)).toBe(28)
+    })
+
+    it('should accept an array as an argument', () => {
+        expect(add([1,2,3,4])).toBe(10)
+        expect(add([])).toBe(0)
+        expect(add([4])).toBe(4)
+    })
+})
